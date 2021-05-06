3c73da4e2358add5e01e3dcb171d03dab9d800a2
tps1147 <tps1147@gmail.com>
2021-05-05 18:13:18 -0600

    email on purchase

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
+var nodemailer = require('nodemailer');
 
 const authRoutes = require('./routes/auth');
 const itemRoutes = require('./routes/item');
@@ -32,6 +33,9 @@ const stripe = require("stripe")("sk_test_51IN7khGVhP2rcc7nk5psNjLWxh8hYDtvdJjrG
 
 function calculateOrderAmount(products) {
   if (products >= 1) {
+    let time = new Date()
+    let hour = time.getHours()
+    console.log(hour)
     let total = products * 100
     return total
   } else {
@@ -51,6 +55,52 @@ app.post("/create-payment-intent", async (req, res) => {
   });
   
 });
+
+
+
+
+
+// email on purchase
+async function mail() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.elasticemail.com",
+    port: 2525,
+    secure: false,
+    service: 'gmail',
+    auth: {
+          user: 'sitepplx@gmail.com',
+          pass: 'Potishot69'
+         } // true for 465, false for other ports,
+  });
+  // E6CA1D42B1FEC0CF1C68DCC67FE1F38A1435
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'sitepplx@gmail.com', // sender address
+    to: "tps1147@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+mail().catch(console.error);
+
+
+
+
+
 
 
 
